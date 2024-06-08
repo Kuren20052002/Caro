272487e25d0f9540b75b69c216d6f2131d31c730
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-06-08 18:53:42 +0700

    make it so that player class handles it own business

---
+++ lib/player.rb
@@ -0,0 +1,33 @@
+class Player
+  attr_reader :name, :symbol
+
+  def initialize
+    @name = whats_your_name
+    @symbol = what_symbol
+  end
+
+  def make_move
+    puts "Make a move: "
+    gets.to_i
+  end
+
+  def whats_your_name
+    puts "What's your name?"
+    loop do
+      name = gets.chomp
+      return name unless name == ""
+
+      puts "Invalid input, please choose again"
+    end
+  end
+
+  def what_symbol
+    puts "Choose your symbol: X or O (X plays first):"
+    loop do
+      symbol = gets.chomp.upcase
+      return symbol if %w[X O].include?(symbol)
+
+      puts "Invalid input, please choose again"
+    end
+  end
+end
